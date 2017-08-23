92c8497b01cdc1b7cfa04077afd66493093fa2e2
HayelomR <rhayelom@gmail.com>
2017-08-22 19:54:34 -0600

    m

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-// create the connection information for the sql database
-var connection = mysql.createConnection({
+var connection; 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "303261",
   database: "burgers_db"
 });
+};
 // connect to the mysql server and sql database
 connection.connect(function(err) {
      if (err) {
